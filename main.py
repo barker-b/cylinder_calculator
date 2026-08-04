8a58203c9ab3c99b3ef409559873555b77cac148
Michael <barkerbjones@outlook.com>
2026-08-03 22:40:33 -0600

    Filled out pump_tab, added a pump formula, need to fix pump_tab to be for pumps..

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
-import calculator as calc
 from button_class import Button
 from cylinder_tab import build_cyl_tab
 from motor_tab import build_motor_tab
+from pump_tab import build_pump_tab
 
 root = tk.Tk()
 root.title("Cylinder Calculator")
@@ -14,6 +14,8 @@ notebook.pack(fill="both", expand=True)
 
 build_cyl_tab(notebook)
 build_motor_tab(notebook)
+build_pump_tab(notebook)
 
 root.mainloop()
+
 
